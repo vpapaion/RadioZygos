5f530a77571b75524e579040612f002864e390a5
vpapaion <63016422+vpapaion@users.noreply.github.com>
2026-05-16 20:41:41 +0300

    Fix crash by using ImageButton and proper MediaController release

--- app/src/main/java/gr/vpapaion/radiozygos/MainActivity.kt
+++ app/src/main/java/gr/vpapaion/radiozygos/MainActivity.kt
@@ -4,19 +4,21 @@ import android.Manifest
 import android.content.ComponentName
 import android.os.Build
 import android.os.Bundle
-import android.widget.Button
+import android.widget.ImageButton
 import android.widget.TextView
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.media3.common.Player
 import androidx.media3.session.MediaController
 import androidx.media3.session.SessionToken
+import com.google.common.util.concurrent.ListenableFuture
 import com.google.common.util.concurrent.MoreExecutors
 
 class MainActivity : AppCompatActivity() {
 
     private var controller: MediaController? = null
-    private lateinit var playPauseButton: Button
+    private var controllerFuture: ListenableFuture<MediaController>? = null
+    private lateinit var playPauseButton: ImageButton
     private lateinit var statusText: TextView
 
     private val requestNotificationPermission = registerForActivityResult(
@@ -41,37 +43,39 @@ class MainActivity : AppCompatActivity() {
             } else {
                 mediaController.play()
             }
-            updateUi(mediaController.isPlaying)
         }
     }
 
     override fun onStart() {
         super.onStart()
         val sessionToken = SessionToken(this, ComponentName(this, RadioPlaybackService::class.java))
-        val controllerFuture = MediaController.Builder(this, sessionToken).buildAsync()
-        controllerFuture.addListener(
-            {
-                controller = controllerFuture.get().also { mediaController ->
-                    mediaController.addListener(object : Player.Listener {
-                        override fun onIsPlayingChanged(isPlaying: Boolean) {
-                            updateUi(isPlaying)
-                        }
-                    })
-                    updateUi(mediaController.isPlaying)
-                }
-            },
-            MoreExecutors.directExecutor()
-        )
+        controllerFuture = MediaController.Builder(this, sessionToken).buildAsync().also { future ->
+            future.addListener(
+                {
+                    controller = future.get().also { mediaController ->
+                        mediaController.addListener(object : Player.Listener {
+                            override fun onIsPlayingChanged(isPlaying: Boolean) {
+                                updateUi(isPlaying)
+                            }
+                        })
+                        updateUi(mediaController.isPlaying)
+                    }
+                },
+                MoreExecutors.directExecutor()
+            )
+        }
     }
 
     override fun onStop() {
-        controller?.let { MediaController.releaseFuture(MediaController.Builder(this, SessionToken(this, ComponentName(this, RadioPlaybackService::class.java))).buildAsync()) }
+        controllerFuture?.let { MediaController.releaseFuture(it) }
+        controllerFuture = null
         controller = null
         super.onStop()
     }
 
     private fun updateUi(isPlaying: Boolean) {
-        playPauseButton.text = if (isPlaying) getString(R.string.pause) else getString(R.string.play)
+        playPauseButton.setImageResource(if (isPlaying) R.drawable.ic_pause else R.drawable.ic_play_arrow)
+        playPauseButton.contentDescription = if (isPlaying) getString(R.string.pause) else getString(R.string.play)
         statusText.text = if (isPlaying) getString(R.string.playing_now) else getString(R.string.ready_to_play)
     }
 }
